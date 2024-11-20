da17147005ae6e45dff75814342cc7da02d15d5e
StefanoMarti24 <martinellistefano354@gmail.com>
2024-11-20 17:55:41 -0300

    PilasEjemplo

---
+++ PilasEjemplo/PilasEjemplo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace PilasEjemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombreNodo = new System.Windows.Forms.TextBox();
+            this.btnAgregarNodo = new System.Windows.Forms.Button();
+            this.lstMiPila = new System.Windows.Forms.ListBox();
+            this.btnDesapilar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // txtNombreNodo
+            // 
+            this.txtNombreNodo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombreNodo.Location = new System.Drawing.Point(93, 23);
+            this.txtNombreNodo.Name = "txtNombreNodo";
+            this.txtNombreNodo.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreNodo.TabIndex = 1;
+            // 
+            // btnAgregarNodo
+            // 
+            this.btnAgregarNodo.Location = new System.Drawing.Point(108, 65);
+            this.btnAgregarNodo.Name = "btnAgregarNodo";
+            this.btnAgregarNodo.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregarNodo.TabIndex = 2;
+            this.btnAgregarNodo.Text = "Apilar";
+            this.btnAgregarNodo.UseVisualStyleBackColor = true;
+            this.btnAgregarNodo.Click += new System.EventHandler(this.btnAgregarNodo_Click);
+            // 
+            // lstMiPila
+            // 
+            this.lstMiPila.FormattingEnabled = true;
+            this.lstMiPila.Location = new System.Drawing.Point(223, 23);
+            this.lstMiPila.Name = "lstMiPila";
+            this.lstMiPila.Size = new System.Drawing.Size(174, 277);
+            this.lstMiPila.TabIndex = 3;
+            // 
+            // btnDesapilar
+            // 
+            this.btnDesapilar.Location = new System.Drawing.Point(108, 127);
+            this.btnDesapilar.Name = "btnDesapilar";
+            this.btnDesapilar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesapilar.TabIndex = 4;
+            this.btnDesapilar.Text = "Desapilar";
+            this.btnDesapilar.UseVisualStyleBackColor = true;
+            this.btnDesapilar.Click += new System.EventHandler(this.btnDesapilar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 330);
+            this.Controls.Add(this.btnDesapilar);
+            this.Controls.Add(this.lstMiPila);
+            this.Controls.Add(this.btnAgregarNodo);
+            this.Controls.Add(this.txtNombreNodo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombreNodo;
+        private System.Windows.Forms.Button btnAgregarNodo;
+        private System.Windows.Forms.ListBox lstMiPila;
+        private System.Windows.Forms.Button btnDesapilar;
+    }
+}
+
